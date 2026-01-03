dcbfe55f185f1da5d209277eb2ac151edb89e085
sgs9906 <saad.guessous@etu.inp-n7.fr>
2026-01-03 01:19:32 +0100

    finallo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/Portfolio', // Le nom exact de ton repo GitHub
   images: { unoptimized: true }
 };
 
